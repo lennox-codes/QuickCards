1d58ef5a6d633a48ec0c67ecffca5cb7c5aebd97
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-22 20:35:30 -0400

    started server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// Load config file
+dotenv.config({ path: "config/config.env" });
+
+app.get("/", (req, res) => {
+  res.send("How are you doing, my dudes and duddetes");
+});
+
+app.listen(
+  process.env.PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+);
