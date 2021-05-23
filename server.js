b16993c7cd301f84dd1b0374acb2cece60d4da06
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-22 20:55:03 -0400

    connected to database

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
 // Load config file
 dotenv.config({ path: "config/config.env" });
 
+// Connect to database
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("How are you doing, my dudes and duddetes");
 });
 
 app.listen(
-  process.env.PORT,
+  process.env.PORT || 8000,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
 );
