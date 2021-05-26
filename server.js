c9314ad1c2486f90de95a949772f45e20c212bc1
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-25 22:49:00 -0400

    added methodOveride and morgan middleware

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 // Load config file
 dotenv.config({ path: "config/config.env" });
@@ -10,11 +15,30 @@ dotenv.config({ path: "config/config.env" });
 // Connect to database
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("How are you doing, my dudes and duddetes");
-});
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Method Override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Morgan
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.listen(
-  process.env.PORT || 8000,
+  process.env.PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
 );
