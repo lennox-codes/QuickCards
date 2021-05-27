a8e1682cce4f31b968eba1299e46a52743adef91
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-26 20:39:27 -0400

    removed deprecation warning

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: true,
+      useCreateIndex: true,
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
