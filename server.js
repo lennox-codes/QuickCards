7c6ff3806dace81e59ef2b1f30ebf778982577b6
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-26 21:08:12 -0400

    added passport config caller

--- server.js
+++ server.js
@@ -12,12 +12,25 @@ const MongoStore = require("connect-mongo");
 // Load config file
 dotenv.config({ path: "config/config.env" });
 
+// Passport config
+require("./config/passport")(passport);
+
 // Connect to database
 connectDB();
 
 // Body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 
 // Method Override
 app.use(
@@ -37,6 +50,13 @@ app.use(express.static(path.join(__dirname, "public")));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Route Processing
+app.use("/auth", require("./routes/auth"));
 
 app.listen(
   process.env.PORT,
