82525f02a95c513e93ec00de268a579e91fb2103
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-31 22:10:24 -0400

    removed UserCard Schema

---
+++ models/Cards.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const CardSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  category: {
+    type: String,
+    enum: ["birthday", "holiday", "congrats", "anniversary"],
+  },
+
+  type: {
+    type: String,
+    enum: ["starter", "user"],
+    default: "starter",
+  },
+
+  content: {
+    image: String,
+    text: [String],
+  },
+
+  recipientName: {
+    type: String,
+  },
+
+  sent: {
+    type: Boolean,
+    default: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Card", CardSchema);
