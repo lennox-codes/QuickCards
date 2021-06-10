97cbe9a07c66975406814635c8364698d1f6b764
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-10 11:20:56 -0400

    added GET endpoints

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,130 +1,24 @@
-const validator = require("validator");
-const { validateRegistration, validateLogin } = require("../../custom-validators/user");
-const passport = require("passport");
-const User = require("../../models/User");
+const Card = require("../models/Card");
 
-/* LOCAL AUTHENTICATION */
-// @desc Process User Login
-// @route POST /auth/login
-const postLogin = async (req, res, next) => {
-  const { validationErrors, isValid } = validateLogin(req.body);
-
+const getAllStarterCards = async (req, res) => {
   try {
-    if (isValid) {
-      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
-
-      passport.authenticate("local-user", (err, user, info) => {
-        if (err) return next(err);
-        if (!user) {
-          return res.json({ error: info });
-          //return res.redirect("/");
-        }
-
-        req.login(user, (err) => {
-          if (err) return next(err);
-          return res.json({ success: { msg: "Success! you have been successfully logged in" } });
-          //return res.redirect("/cards");
-        });
-      })(req, res, next);
-    } else {
-      return res.json(validationErrors);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// @desc Process User Registration
-// @route POST /auth/register
-const postRegistration = async (req, res, next) => {
-  const { validationErrors, isValid } = validateRegistration(req.body);
-
-  try {
-    if (isValid) {
-      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
-
-      const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-      });
-
-      const user = await User.findOne({ email: req.body.email });
-
-      if (user) {
-        return res.json({ msg: "Account with that email address or username already exists" });
-      }
-
-      await newUser.save();
-
-      req.login(newUser, function (err) {
-        if (err) return next(err);
-        return res.redirect("/");
-      });
-    } else {
-      console.log(validationErrors);
-      return res.json(validationErrors);
-    }
+    const cards = await Card.find({ type: "starter" });
+    if (cards) res.json(cards);
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
 
-const getSignUp = () => {};
-
-//@desc Auth with Google
-//@route GET/ auth/google
-const getGoogleLogIn = (req, res, next) => {
-  passport.authenticate("google", { scope: ["email", "profile"] })(req, res, next);
-};
-
-// @desc Google with callback
-// @route GET/ auth/google/callback
-const getGoogleCallback = (req, res, next) => {
-  passport.authenticate("google", { failureRedirect: "/" })(req, res, next);
-  res.json({ success: { msg: "Google Oauth Sucessful" } });
-};
-
-// @desc Login Admin
-// @router admin/login
-// Essential to enable me to be able to post new cards from time to time
-const postAdminLogin = async (req, res, next) => {
-  const { validationErrors, isValid } = validateLogin(req.body);
-
+const getSingleCard = async (req, res) => {
+  const cardId = req.params.cardId;
   try {
-    if (isValid) {
-      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
-
-      passport.authenticate("local-admin", (err, user, info) => {
-        if (err) return next(err);
-        if (!user) {
-          return res.json({ error: info });
-          //return res.redirect("/");
-        }
-
-        req.login(user, (err) => {
-          if (err) return next(err);
-          return res.json({ success: { msg: "Success! you have been successfully logged in" } });
-          //return res.redirect("/cards");
-        });
-      })(req, res, next);
-    } else {
-      return res.json(validationErrors);
-    }
-  } catch (err) {
-    console.log(err);
+    const card = await Card.findById(cardId);
+    if (card) res.json(card);
+  } catch (error) {
+    console.log(error);
   }
 };
 
-// @desc Logout user
-// @route GET /auth/logout
-const logOut = (req, res) => {
-  req.logout();
-  return res.json({ msg: "Log out Successful" });
-  //res.redirect("/");
-};
+const createNewCard = async (req, res) => {};
 
-const localAuthController = { postRegistration, postLogin, postAdminLogin };
-const openAuthController = { getGoogleLogIn, getGoogleCallback };
-module.exports = { localAuthController, openAuthController, logOut };
+module.exports = { getAllStarterCards, getSingleCard, createNewCard };
