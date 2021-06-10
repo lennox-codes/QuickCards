a58c963052ac821f849b885913454decd9002c9c
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-10 11:58:22 -0400

    Revert "first react-app commit"
    This reverts commit 39f3fa22eba52551867b487175592df08657f57c.

--- backend/routes/index.js
+++
@@ -1,5 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const homeController = require("../controllers/home");
-
-//This might display the dashboard showing the cards that the user recieved and the cards that the user has contributed to and whatnot.
