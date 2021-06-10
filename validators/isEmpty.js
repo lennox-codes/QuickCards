551faa884a3f7ecd708cf6cbfca4178f8035f672
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-10 14:01:35 -0400

    organized structure

--- validators/isEmpty.js
+++
@@ -1,7 +0,0 @@
-const isEmpty = (value) => {
-  !value ||
-    (typeof value === "object" && Boolean(Object.keys(value).length)) ||
-    (typeof value === "string" && Boolean(value.trim().length));
-};
-
-module.exports = isEmpty;
